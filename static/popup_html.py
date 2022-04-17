262f17e676a3518825926e9af95b842af6a38e1f
Albert Hsueh <ahsueh@Alberts-MacBook-Pro.local>
2022-04-17 11:41:08 -0400

    Added additional fields to popup

--- static/popup_html.py
+++ static/popup_html.py
@@ -3,6 +3,11 @@ def popup_html(row_num, df_airbnb):
     name=df_airbnb['name'].iloc[i]
     price=df_airbnb['price'].iloc[i]
     roomtype=df_airbnb['room_type'].iloc[i]
+    accom=df_airbnb['accommodates'].iloc[i]
+    beds=int(df_airbnb['beds'].iloc[i])
+    baths=df_airbnb['bathrooms_text'].iloc[i]
+    link=df_airbnb['listing_url'].iloc[i]
+
 
 
     left_col_color = "#19a7bd"
@@ -12,7 +17,7 @@ def popup_html(row_num, df_airbnb):
 <html>
 
 <head>
-<h4 style="margin-bottom:10"; width="200px">{}</h4>""".format(name) + """
+<h4 style="margin-bottom:10"; width="200px"><a href="{}">{}</a></h4>""".format(link, name) + """
 
 </head>
     <table style="height: 20px; width: 200px; border:1px solid black">
@@ -25,6 +30,21 @@ def popup_html(row_num, df_airbnb):
 <tr style = "border:1px solid black">
 <td style = "border:1px solid black">Room Type:</td>
 <td style = "border:1px solid black">&nbsp; {}</td>""" .format(roomtype) + """
+</tr>
+
+<tr style = "border:1px solid black">
+<td style = "border:1px solid black">Accommodates:</td>
+<td style = "border:1px solid black">&nbsp; {}</td>""" .format(accom) + """
+</tr>
+
+<tr style = "border:1px solid black">
+<td style = "border:1px solid black">Beds:</td>
+<td style = "border:1px solid black">&nbsp; {}</td>""" .format(beds) + """
+</tr>
+
+<tr style = "border:1px solid black">
+<td style = "border:1px solid black">Baths:</td>
+<td style = "border:1px solid black">&nbsp; {}</td>""" .format(baths) + """
 </tr>
 
 </tbody>
